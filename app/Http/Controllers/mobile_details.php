fcfb1d1223814daf4e4b06e601f59eb2e073c0ac
Priya8632 <pthummar775@gmail.com>
2022-04-06 19:43:23 +0530

    add

--- app/Http/Controllers/mobile_details.php
+++ app/Http/Controllers/mobile_details.php
@@ -8,11 +8,6 @@ class mobile_details extends Controller
 {
     function mobile($value)
     {
-        $vivo = array("color"=>"black","Ram"=>"4");
-
-        if($value == $vivo)
-        {
-            echo $vivo['color'];
-        }
+       
     }
 }
